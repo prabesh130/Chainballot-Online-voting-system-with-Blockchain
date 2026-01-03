7a81c9eeb517de44a406d8d146076e7510a5bfe0
prabesh130 <adhikariprabesh828@gmail.com>
2026-01-03 15:04:00 +0545

    changes made to fit the frontend end points

--- Backend/chainballot/settings.py
+++ Backend/chainballot/settings.py
@@ -38,9 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +130,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',),
+}
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+    'ROTEN_REFRESH_TOKENS':False,
+    'BLACKLIST_AFTER_ROTATION':False,
+    'UPDATE_LAST_LOGIN':False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,  # Uses your Django SECRET_KEY
+    'VERIFYING_KEY': None,
+    
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+}
